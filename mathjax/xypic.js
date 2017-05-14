4c2323c5b942a6c8b220b267cbd9ac34b8ec12d6
genkuroki <genkuroki@gmail.com>
2017-05-14 12:43:18 +0900

    XyJax

--- mathjax/xypic.js
+++ mathjax/xypic.js
@@ -16769,4 +16769,4 @@ MathJax.Hub.Register.StartupHook("SVG Xy-pic Require",function () {
   MathJax.Hub.Startup.signal.Post("SVG Xy-pic Ready");
 });
 
-MathJax.Ajax.loadComplete("[Contrib]/xypic.js");
+MathJax.Ajax.loadComplete("[Local]/xypic.js");
